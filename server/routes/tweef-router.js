28aee309c8f9797522bedb2c0b4e20f2d4890ef5
Lilith <valerie.vaske@gmail.com>
2020-04-28 01:32:08 +0200

    Updated Tweef-Router variables

--- server/routes/tweef-router.js
+++ server/routes/tweef-router.js
@@ -5,9 +5,13 @@ const TweefCtrl = require('../controllers/tweef-ctrl')
 const router = express.Router()
 
 router.post('/tweef', TweefCtrl.createTweef)
-router.put('/tweef/:id', TweefCtrl.updateTweef)
 router.delete('/tweef/:id', TweefCtrl.deleteTweef)
-router.get('/tweef/:id', TweefCtrl.getTweefById)
-router.get('/tweefs', TweefCtrl.getTweefs)
+
+router.put('/tweef/:user', TweefCtrl.addFollowsToUser)
+router.delete('tweef/:user', TweefCtrl.removeFollowsFromUser)
+router.put('tweef/:user', TweefCtrl.addTweetToUser)
+router.delete('tweef/:user', TweefCtrl.removeTweetFromUser)
+router.get('tweef/:user', TweefCtrl.getFollowsByUser)
+router.get('tweef/:user', TweefCtrl.getTweetsByUser)
 
 module.exports = router
