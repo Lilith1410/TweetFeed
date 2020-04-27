61d442afade4a371d77d2b489a184e9a053b448d
Lilith <valerie.vaske@gmail.com>
2020-04-28 01:32:24 +0200

    Updates Tweef-Controller (untested!)

--- server/controllers/tweef-ctrl.js
+++ server/controllers/tweef-ctrl.js
@@ -1,5 +1,6 @@
 const Tweef = require('../models/tweef-model')
 
+{/*Check this again to make sure it works*/}
 
 createTweef = (req, res) => {
   const body = req.body
@@ -7,7 +8,7 @@ createTweef = (req, res) => {
   if (!body) {
     return res.status(400).json({
       success: false,
-      error: 'You must provide a Tweef',
+      error: 'Must provide User. ',
     })
   }
 
@@ -23,54 +24,15 @@ createTweef = (req, res) => {
       return res.status(201).json({
         success: true,
         id: tweef._id,
-        message: 'Tweef created!',
+        message: 'User created.',
       })
     })
     .catch(error => {
       return res.status(400).json({
         error,
-        message: 'Tweef not created!',
+        message: 'User not created. ',
       })
     })
-}
-
-updateTweef = async (req, res) => {
-  const body = req.body
-
-  if(!body) {
-    return res.status(400).json({
-      success: false,
-      error: 'You must provide a body to update',
-    })
-  }
-
-  Tweef.findOne({_id: req.params.id}, (err, tweef) => {
-    if(err) {
-      return res.status(404).json({
-        err,
-        message: 'Tweef not found!',
-      })
-    }
-
-    tweef.name = body.name
-    tweef.time = body.time
-    tweef.rating = body.rating
-    tweef
-      .save()
-      .then(() => {
-        return res.status(200).json({
-          success: true,
-          id: tweef._id,
-          message: 'Tweef updated!',
-        })
-      })
-      .catch(error => {
-        return res.status(404).json({
-          error,
-          message: 'Tweef not updated',
-        })
-      })
-  })
 }
 
 deleteTweef = async (req, res) => {
@@ -82,48 +44,220 @@ deleteTweef = async (req, res) => {
     if(!tweef) {
       return res
         .status(404)
-        .json({ success: false, error: 'Tweef not found' })
+        .json({ success: false, error: 'User not found' })
     }
 
     return res.status(200).json({ success: true, data: tweef})
   }).catch(err => console.log(err))
 }
 
+addFollowsToUser = async (req, res) => {
+  const body = req.body
 
-getTweefById = async (req, res) => {
-    await Tweef.findOne({ _id: req.params.id }, (err, tweef) => {
-        if (err) {
-            return res.status(400).json({ success: false, error: err })
-        }
+  if(!body) {
+    return res.status(400).json({
+      success: false,
+      error: 'You must provide a user to update',
+    })
+  }
 
-        if (!tweef) {
-            return res
-                .status(404)
-                .json({ success: false, error: `Tweef not found` })
-        }
-        return res.status(200).json({ success: true, data: tweef })
-    }).catch(err => console.log(err))
+  Tweef.findOne({_id: req.params.id}, (err, tweef) => {
+    if(err) {
+      return res.status(404).json({
+        err,
+        message: 'User not found!',
+      })
+    }
+
+    {/*Would also require a check that the data in the body is actually a valid user in the db*/}
+    {/*Unsure if this work - test! */}
+    tweef.follows.push(body.follows)
+
+    tweef
+      .save()
+      .then(() => {
+        return res.status(200).json({
+          success: true,
+          id: tweef._id,
+          message: 'Follows for User' + req.params.id + ' updated!',
+        })
+      })
+      .catch(error => {
+        return res.status(404).json({
+          error,
+          message: 'Follows not updated',
+        })
+      })
+  })
 }
 
-getTweefs = async (req, res) => {
-    await Tweef.find({}, (err, tweefs) => {
-        if (err) {
-            return res.status(400).json({ success: false, error: err })
-        }
-        if (!tweefs.length) {
-            return res
-                .status(404)
-                .json({ success: false, error: `Tweefs not found` })
-        }
-        return res.status(200).json({ success: true, data: tweefs })
-    }).catch(err => console.log(err))
+removeFollowsFromUser = async (req, res) => {
+  const body = req.body
+
+  if(!body) {
+    return res.status(400).json({
+      success: false,
+      error: 'You must provide a user to update',
+    })
+  }
+
+  Tweef.findOne({_id: req.params.id}, (err, tweef) => {
+    if(err) {
+      return res.status(404).json({
+        err,
+        message: 'User not found. ',
+      })
+    }
+
+    {/*Would also require a check that the data in the body is actually a valid user in the db*/}
+    {/*Unsure if this work - test! */}
+    for(var i = 0; i < tweef.follows.length; i++) {
+      {/*Geht dieser Vergleich überhaupt klar? Oder hat er damit probleme?*/}
+      if(tweef.follows[i] == body.follows) {
+        tweef.follows.splice(i, 1);
+      }
+    }
+
+    tweef
+      .save()
+      .then(() => {
+        return res.status(200).json({
+          success: true,
+          id: tweef._id,
+          message: 'Follows for User' + req.params.id + ' deleted. ',
+        })
+      })
+      .catch(error => {
+        return res.status(404).json({
+          error,
+          message: 'Follows not deleted. ',
+        })
+      })
+  })
 }
 
+getFollowsByUser = async (req, res) => {
+  await Tweef.findOne({ _user: req.params.user }, (err, tweef) => {
+      if (err) {
+          return res.status(400).json({ success: false, error: err })
+      }
+
+      if (!tweef) {
+          return res
+              .status(404)
+              .json({ success: false, error: `Follows not found` })
+      }
+      return res.status(200).json({ success: true, data: tweef })
+  }).catch(err => console.log(err))
+}
+
+addTweetToUser = async (req, res) => {
+  const body = req.body
+
+  if(!body) {
+    return res.status(400).json({
+      success: false,
+      error: 'You must provide a user to update',
+    })
+  }
+
+  Tweef.findOne({_id: req.params.id}, (err, tweef) => {
+    if(err) {
+      return res.status(404).json({
+        err,
+        message: 'User not found!',
+      })
+    }
+
+    {/*Would also require a check that the data in the body is actually a valid user in the db*/}
+    {/*Unsure if this work - test! */}
+    tweef.tweets.push(body.tweets)
+
+    tweef
+      .save()
+      .then(() => {
+        return res.status(200).json({
+          success: true,
+          id: tweef._id,
+          message: 'Tweet for User' + req.params.id + ' updated!',
+        })
+      })
+      .catch(error => {
+        return res.status(404).json({
+          error,
+          message: 'Tweets not updated',
+        })
+      })
+  })
+}
+
+removeTweetFromUser = async (req, res) => {
+  const body = req.body
+
+  if(!body) {
+    return res.status(400).json({
+      success: false,
+      error: 'You must provide a user to update',
+    })
+  }
+
+  Tweef.findOne({_id: req.params.id}, (err, tweef) => {
+    if(err) {
+      return res.status(404).json({
+        err,
+        message: 'User not found. ',
+      })
+    }
+
+    {/*Would also require a check that the data in the body is actually a valid user in the db*/}
+    {/*Unsure if this work - test! */}
+    for(var i = 0; i < tweef.tweets.length; i++) {
+      {/*Geht dieser Vergleich überhaupt klar? Oder hat er damit probleme?*/}
+      if(tweef.tweets[i] == body.tweets) {
+        tweef.tweets.splice(i, 1);
+      }
+    }
+
+    tweef
+      .save()
+      .then(() => {
+        return res.status(200).json({
+          success: true,
+          id: tweef._id,
+          message: 'Tweet for User' + req.params.id + ' deleted. ',
+        })
+      })
+      .catch(error => {
+        return res.status(404).json({
+          error,
+          message: 'Tweet not deleted. ',
+        })
+      })
+  })
+}
+
+getTweetsByUser = async (req, res) => {
+  await Tweef.findOne({ _user: req.params.user }, (err, tweef) => {
+      if (err) {
+          return res.status(400).json({ success: false, error: err })
+      }
+
+      if (!tweef) {
+          return res
+              .status(404)
+              .json({ success: false, error: `Tweets not found` })
+      }
+      return res.status(200).json({ success: true, data: tweef })
+  }).catch(err => console.log(err))
+}
 
 module.exports = {
   createTweef,
-  updateTweef,
   deleteTweef,
-  getTweefs,
-  getTweefById,
+  addFollowsToUser,
+  removeFollowsFromUser,
+  addTweetToUser,
+  removeTweetFromUser,
+  getFollowsByUser,
+  getTweetsByUser
 }
