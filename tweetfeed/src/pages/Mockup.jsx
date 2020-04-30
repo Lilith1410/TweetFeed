e8a4c37f7eb32ae0d49ad4bbba51cfac74d10851
Lilith <valerie.vaske@gmail.com>
2020-05-01 01:32:41 +0200

    Updated Mockup with Users List

--- tweetfeed/src/pages/Mockup.jsx
+++ tweetfeed/src/pages/Mockup.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+// using table only for the moment. change to drop down as soon as possible!
+import ReactTable from 'react-table'
 import api from '../api'
 
 import styled from 'styled-components'
@@ -13,16 +15,64 @@ class Mockup extends Component {
     super(props)
 
     this.state = {
-      id: ''
+        users: [],
+        columns: [],
+        isLoading: false,
     }
   }
 
+  componentDidMount = async () => {
+      this.setState({ isLoading: true })
+
+      await api.getAllTweefs().then(tweefs => {
+          this.setState({
+              users: tweefs.data.data,
+              isLoading: false,
+          })
+      })
+  }
+
+
+
   render() {
+
+    const { users, isLoading } = this.state
+    console.log('TCL: UserList -> render -> users', users)
+
+    const columns = [
+      {
+          Header: 'ID',
+          accessor: '_id',
+          filterable: true,
+      },
+      {
+          Header: 'userName',
+          accessor: 'userName',
+          filterable: true,
+      }
+    ]
+
+    let showTable = true
+    if (!users.length) {
+        showTable = false
+    }
+
     return (
       <Wrapper>
         <p>This is the Mockup Page! Here shall at one point be: </p>
         <br />
         <p> User Selection via Drop Down. Handing over the user id to UserProfile Page. And Drag&Drop Input of .txt-Files for DB Initialization.  </p>
+        <br />
+        {showTable && (
+            <ReactTable
+                data={users}
+                columns={columns}
+                loading={isLoading}
+                defaultPageSize={10}
+                showPageSizeOptions={true}
+                minRows={0}
+            />
+        )}
       </Wrapper>
 
     )
