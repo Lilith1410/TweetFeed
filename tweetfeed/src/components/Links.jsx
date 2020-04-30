c9a28bd33293c3b98179a55c0fea2225d97ca44f
Lilith <valerie.vaske@gmail.com>
2020-05-01 00:13:21 +0200

    Updated Application Title in Frontend/Navbar

--- tweetfeed/src/components/Links.jsx
+++ tweetfeed/src/components/Links.jsx
@@ -19,7 +19,7 @@ class Links extends Component {
         return (
             <React.Fragment>
                 <Link to="/" className="navbar-brand">
-                    My first MERN Application
+                    TweetFeed
                 </Link>
                 <Collapse>
                     <List>
