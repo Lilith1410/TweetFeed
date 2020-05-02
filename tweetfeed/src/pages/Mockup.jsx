9f778615e0b044aa2fc0a2fe9109b67c0299cf81
Lilith <valerie.vaske@gmail.com>
2020-05-02 14:46:15 +0200

    Working - Updated Mockup Page to direct to User Profile

--- tweetfeed/src/pages/Mockup.jsx
+++ tweetfeed/src/pages/Mockup.jsx
@@ -9,35 +9,45 @@ const Wrapper = styled.div`
     padding: 0 40px 40px 40px;
 `
 
+const Click = styled.div`
+    cursor: pointer;
+`
+
+class ClickUserProfile extends Component {
+  clickUser = event => {
+    event.preventDefault()
+
+    window.location.href = `/tweefs/user/${this.props.id}`
+  }
+
+  render() {
+    return <Click onClick={this.clickUser}>Profile</Click>
+  }
+}
+
 class Mockup extends Component {
 
   constructor(props) {
     super(props)
 
     this.state = {
-        users: [],
+        tweefs: [],
         columns: [],
-        isLoading: false,
     }
   }
 
   componentDidMount = async () => {
-      this.setState({ isLoading: true })
-
-      await api.getAllTweefs().then(tweefs => {
-          this.setState({
-              users: tweefs.data.data,
-              isLoading: false,
-          })
+    await api.getAllTweefs().then(tweefs => {
+      this.setState({
+        tweefs: tweefs.data.data
       })
+    })
   }
-
-
 
   render() {
 
-    const { users, isLoading } = this.state
-    console.log('TCL: UserList -> render -> users', users)
+    const { tweefs } = this.state
+    console.log('TCL: UserList -> render -> users', tweefs)
 
     const columns = [
       {
@@ -49,11 +59,22 @@ class Mockup extends Component {
           Header: 'userName',
           accessor: 'userName',
           filterable: true,
-      }
+      },
+      {
+          Header: '',
+          accessor: '',
+          Cell: function(props) {
+              return (
+                  <span>
+                      <ClickUserProfile id={props.original._id} userName={props.original.userName} />
+                  </span>
+              )
+          },
+      },
     ]
 
     let showTable = true
-    if (!users.length) {
+    if (!tweefs.length) {
         showTable = false
     }
 
@@ -65,9 +86,8 @@ class Mockup extends Component {
         <br />
         {showTable && (
             <ReactTable
-                data={users}
+                data={tweefs}
                 columns={columns}
-                loading={isLoading}
                 defaultPageSize={10}
                 showPageSizeOptions={true}
                 minRows={0}
