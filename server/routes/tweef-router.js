f5472857b9a012f3cb8450ded18e2bcc60789952
Lilith <valerie.vaske@gmail.com>
2020-05-04 01:07:20 +0200

    Working - Backend: Added DropDatabase

--- server/routes/tweef-router.js
+++ server/routes/tweef-router.js
@@ -10,5 +10,6 @@ router.delete('/tweef/:id', TweefCtrl.deleteTweef)
 router.get('/tweef/:id', TweefCtrl.getTweefById)
 router.get('/tweefs', TweefCtrl.getTweefs)
 router.get('/tweef', TweefCtrl.getTweefByName)
+router.delete('/tweefs', TweefCtrl.deleteAllTweefs)
 
 module.exports = router
