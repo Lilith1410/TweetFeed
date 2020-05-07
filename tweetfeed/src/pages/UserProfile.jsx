60ecd431209f2342a31600d6088ece69be0eb137
Lilith <valerie.vaske@gmail.com>
2020-05-08 01:55:56 +0200

    Working - Updated UserProfile Page

--- tweetfeed/src/pages/UserProfile.jsx
+++ tweetfeed/src/pages/UserProfile.jsx
@@ -3,9 +3,69 @@ import api from '../api'
 
 import styled from 'styled-components'
 
+
 const Wrapper = styled.div`
     padding: 0 40px 40px 40px;
 `
+
+const Update = styled.div`
+    color: #ef9b0f;
+    cursor: pointer;
+`
+
+const Delete = styled.div`
+    color: #ff0000;
+    cursor: pointer;
+`
+
+const Button = styled.button.attrs({
+    className: `btn btn-primary`,
+})`
+    margin: 15px 15px 15px 5px;
+`
+
+class UpdateTweef extends Component {
+  updateUser = event => {
+    //event.preventDefault()
+
+    window.location.href = `/tweefs/user/${this.props.id}`
+  }
+
+  render() {
+    return <Update onClick={this.updateUser}>Update</Update>
+  }
+}
+
+class DeleteTweef extends Component {
+  deleteUser = event => {
+    event.preventDefault()
+
+    if(
+      window.confirm(
+        `Do tou want to delete the tweef ${this.props.userName} permanently?`,
+      )
+    ) {
+      api.deleteTweefById(this.props.id)
+      window.location.reload()
+    }
+  }
+
+  render() {
+    return <Delete onClick={this.deleteUser}>Delete</Delete>
+  }
+}
+
+class ClickFollows extends Component {
+  clickUser = event => {
+    event.preventDefault()
+
+    window.location.href = `/tweefs/user/${this.props.id}/follows`
+  }
+
+  render() {
+    return <Button onClick={this.clickUser}>Follows</Button>
+  }
+}
 
 class UserProfile extends Component {
 
@@ -18,6 +78,21 @@ class UserProfile extends Component {
       follows: [],
       tweets: [],
     }
+  }
+
+  handleChangeInputUserName = async event => {
+    const userName = event.target.value
+    this.setState({ userName })
+   }
+
+  handleChangeInputTweets = async event => {
+    const tweets = event.target.value
+    this.setState({ tweets })
+  }
+
+  handleChangeInputFollows = async event => {
+    const follows = event.target.value
+    this.setState({ follows })
   }
 
   componentDidMount = async () => {
@@ -33,13 +108,20 @@ class UserProfile extends Component {
 
   render() {
     const { id, userName, follows, tweets } = this.state
+
     return (
       <Wrapper>
-        <p>This is the User Profile Page! </p>
         <p>Id: {id}</p>
         <p>Username: {userName}</p>
+        <ClickFollows id={id} userName={userName} />
         <p>Follows: {follows}</p>
         <p>Tweets: {tweets}</p>
+        <span>
+            <DeleteTweef userName={userName} />
+        </span>
+        <span>
+            <UpdateTweef id={id} />
+        </span>
       </Wrapper>
 
     )
